3c7fbdb888c845665961f999234bfcebd476b195
Raimon Grau <raimonster@gmail.com>
2021-02-14 10:05:41 +0000

    split databases
    * add network
    * add app-db
    * add data-db
    * support for h2?

--- src/main.clj
+++ src/main.clj
@@ -6,109 +6,116 @@
    [clojure.repl :refer :all]
    [clojure.core :refer :all]
    [clojure.tools.cli :refer [parse-opts]]
+   [babashka.process :refer [$ check process]]
    [clj-yaml.core :as yaml]
    [clojure.string :as str]))
 
+(def databases {:imagemysql57
+                {:image "circleci/mysql:5.7.23"
+                 :user "root"
+                 :volumes ["/home/rgrau/.mba/home/:/root/"]
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+
+                :mongodb
+                {:image "circleci/mongo:4.0"
+                 :user "root"
+                 :volumes ["/home/rgrau/.mba/home/:/root/"]
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+
+                :mariadb-latest
+                {:image "mariadb:latest"
+                 :volumes ["/home/rgrau/.mba/home/:/root/"]
+                 :environment
+                 {:MYSQL_DATABASE "metabase_test"
+                  :MYSQL_USER "root"
+                  :MYSQL_ALLOW_EMPTY_PASSWORD "yes"}
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+
+                :presto
+                {:image "metabase/presto-mb-ci"
+                 :volumes ["/home/rgrau/.mba/home/:/root/"]
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+
+                :sparksql
+                {:image "metabase/spark:2.1.1"
+                 :volumes ["/home/rgrau/.mba/home/:/root/"]
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+
+                :sqlserver
+                {:image "mcr.microsoft.com/mssql/server:2017-latest"
+                 :environment
+                 {:ACCEPT_EULA "Y"
+                  :SA_PASSWORD "P@ssw0rd"}
+                 :volumes ["/home/rgrau/.mba/home/:/root/"]
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+
+                :vertica
+                {:image "sumitchawla/vertica"
+                 :environment
+                 {:ACCEPT_EULA "Y"
+                  :SA_PASSWORD "P@ssw0rd"}
+                 :volumes ["/home/rgrau/.mba/home/:/root/"]
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+
+                :postgres
+                {:image "postgres:12"
+                 :user "root"
+                 :volumes ["/home/rgrau/.mba/home/:/root/"]
+                 :environment
+                 {:POSTGRES_USER "rgrau"
+                  :POSTGRES_PASSWORD "rgrau"
+                  :POSTGRES_DB "rgrau"
+                  :POSTGRES_HOST_AUTH_METHOD "trust"}
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+
+                :mysql
+                {:image "circleci/mysql:5.7.23"
+                 :environment
+                 {:user "root"
+                  :database "circle_test"}
+                 :restart "on-failure"
+                 :stdin_open true
+                 :tty true
+                 :networks ["d"]
+                 :labels {"com.metabase.d" true}}
+  )
+
 (def docker-compose {:version "3.5"
+                     :networks {:d nil}
                      :services
-                     { ;; :testing
-
-                      ;; {:image "ubuntu"
-                      ;;  :user "root"
-                      ;;  :command "tail -f /dev/null"
-                      ;;  :networks ["d"]
-                      ;;  :labels {"com.metabase.d" true}}
-
-                      ;; :imagemysql57
-                      ;; {:image "circleci/mysql:5.7.23"
-                      ;;  :user "root"
-                      ;;  :volumes ["/home/rgrau/.mba/home/:/root/"]
-                      ;;  :restart "on-failure"
-                      ;;  :stdin_open true
-                      ;;  :tty true
-                      ;;  :networks ["d"]
-                      ;;  :labels {"com.metabase.d" true}
-                      ;;  ;; :x-extra {:conn-uri "jdbc:mysql://localhost:3307/circle_test?user=root"}
-                      ;;  }
-
-                      ;; :mongodb
-                      ;; {:image "circleci/mongo:4.0"
-                      ;;  :user "root"
-                      ;;  :volumes ["/home/rgrau/.mba/home/:/root/"]
-                      ;;  :restart "on-failure"
-                      ;;  :stdin_open true
-                      ;;  :tty true
-                      ;;  :networks ["d"]
-                      ;;  :labels {"com.metabase.d" true}}
-
-                      ;; :mariadb-latest
-                      ;; {:image "mariadb:latest"
-                      ;;  :volumes ["/home/rgrau/.mba/home/:/root/"]
-                      ;;  :environment
-                      ;;  {:MYSQL_DATABASE "metabase_test"
-                      ;;   :MYSQL_USER "root"
-                      ;;   :MYSQL_ALLOW_EMPTY_PASSWORD "yes"}
-                      ;;  :restart "on-failure"
-                      ;;  :stdin_open true
-                      ;;  :tty true
-                      ;;  :networks ["d"]
-                      ;;  :labels {"com.metabase.d" true}}
-
-                      :presto
-                      {:image "metabase/presto-mb-ci"
-                       :volumes ["/home/rgrau/.mba/home/:/root/"]
-                       :restart "on-failure"
-                       :stdin_open true
-                       :tty true
-                       :networks ["d"]
-                       :labels {"com.metabase.d" true}}
-
-                      :sparksql
-                      {:image "metabase/spark:2.1.1"
-                       :volumes ["/home/rgrau/.mba/home/:/root/"]
-                       :restart "on-failure"
-                       :stdin_open true
-                       :tty true
-                       :networks ["d"]
-                       :labels {"com.metabase.d" true}}
-
-                      :sqlserver
-                      {:image "mcr.microsoft.com/mssql/server:2017-latest"
-                       :environment
-                       {
-                        :ACCEPT_EULA "Y"
-                        :SA_PASSWORD "P@ssw0rd"
-                        }
-                       :volumes ["/home/rgrau/.mba/home/:/root/"]
-                       :restart "on-failure"
-                       :stdin_open true
-                       :tty true
-                       :networks ["d"]
-                       :labels {"com.metabase.d" true}}
-
-                      :vertica
-                      {:image "sumitchawla/vertica"
-                       :environment
-                       {
-                        :ACCEPT_EULA "Y"
-                        :SA_PASSWORD "P@ssw0rd"
-                        }}
-                      :postgres
-                      {:image "postgres:12"
-                       :user "root"
-                       :volumes ["/home/rgrau/.mba/home/:/root/"]
-                       :environment
-                       {:POSTGRES_USER "rgrau"
-                        :POSTGRES_PASSWORD "rgrau"
-                        :POSTGRES_DB "rgrau"
-                        :POSTGRES_HOST_AUTH_METHOD "trust"}
-                       :restart "on-failure"
-                       :stdin_open true
-                       :tty true
-                       :networks ["d"]
-                       :labels {"com.metabase.d" true}}
-
-                      :maildev
+                     {:maildev
                       {:image "maildev/maildev"
                        ;; :ports ["1080:80", "1025:25"]
                        :ports ["80", "25"]}
@@ -121,28 +128,14 @@
                                  "/home/rgrau/.mba/node_modules/:/root/node_modules/"
                                  (str (System/getProperty "user.dir") ":/app/source/")]
 
-                       :environment {:MB_DB_CONNECTION_URI
-                                     "jdbc:postgresql://postgres:5432/rgrau?user=rgrau&password=rgrau"}
+                       :environment {}
                        :tty true
                        :stdin_open true
                        :restart "on-failure"
                        :command "tail -f /dev/null"
                        :ports ["3000:3000" "8080:8080" "7888:7888"]
                        :networks ["d"]
-                       :labels {"com.metabase.d" true}}
-
-                      ;; :mysql
-                      ;; {:image "circleci/mysql:5.7.23"
-                      ;;  :environment
-                      ;;  {:user "root"
-                      ;;   :database "circle_test"}
-                      ;;  :restart "on-failure"
-                      ;;  :stdin_open true
-                      ;;  :tty true
-                      ;;  :networks ["d"]
-                      ;;  :labels {"com.metabase.d" true}}
-                      }
-                     :networks {:d nil}
+                       :labels {"com.metabase.d" true}}}
                      })
 
 (def cli-options
@@ -150,17 +143,16 @@
     :default 80
     :parse-fn #(Integer/parseInt %)
     :validate [#(< % 0x10000) "Must be between 0 and 65536"]]
-   ["-p" "--prefix PREFIX" "Prefix of docker-compose run"
-    :default "d"]
+   ["-p" "--prefix PREFIX" "Prefix of docker-compose run" :default "d"]
+   ["-n" "--network NETWORK" "network name" :default nil]
    ["-d" "--app-db APP-DB"
     :default "h2"
     :parse-fn (comp keyword str/lower-case)
-    :validate [#{:h2 :postgres :postgresql :mysql}]]
+    :validate [#{:h2 :postgres :postgresql :mysql :mariadb-latest}]]
    ["-D" "--data-db APP-DB"
     :default "postgresql"
     :parse-fn (comp keyword str/lower-case)
-    :validate [#{:postgres :postgresql :mysql :mongo}]]
-   ])
+    :validate [#{:postgres :postgresql :mysql :mongo}]]])
 
 (defmulti task first)
 
@@ -171,43 +163,61 @@
   dc
   )
 
-(defn docker-compose-yml []
-
+(defn docker-compose-yml [docker-compose]
   (yaml/generate-string (massage-format docker-compose) :dumper-options {:flow-style :block}))
 
 (defn docker-compose-yml-file!
-  []
-  (spit my-temp-file
-        (docker-compose-yml))
+  [docker-compose-yml]
+  (spit my-temp-file docker-compose-yml)
   ;; (with-open [file (clojure.java.io/writer my-temp-file)]
   ;;  (binding [*out* file]
   ;;    (println
   ;;     (yaml/generate-string docker-compose :dumper-options {:flow-style :block}))))
   )
 
-(defmethod task :up
-  [args]
-  (docker-compose-yml-file!)
-   ;(sh/sh "docker-compose" "-f" (format "%s" my-temp-file) "up")
-                                        ; (println "up" )
-  (println (docker-compose-yml))
-  )
+(def all-dbs {:postgres "jdbc:postgresql://postgres:5432/rgrau?user=rgrau&password=rgrau"
+              :mariadb-latest "jdbc:mysql://mariadb-latest:3306/metabase_test?user=root"})
 
-(defmethod task :help
-  [args]
-  (println "help!"))
+(defmethod task :up
+  [[cmd opts]]
+  (let [app-db (keyword (:app-db opts))]
+    (-> (cond-> docker-compose
+            (not= :h2 app-db)
+          (assoc-in [:services :metabase :environment :MB_DB_CONNECTION_URI]
+                    (app-db all-dbs))
+          (not= :h2 app-db)
+          (assoc-in [:services app-db] (app-db databases))
+
+          (not (nil? (:network opts)))
+          (assoc-in [:networks :d] {:name (:network opts)}))
+        docker-compose-yml
+        docker-compose-yml-file!))
+  (->
+      ($ ^{:out :inherit :err :inherit} docker-compose -f ~my-temp-file config))
+  nil)
 
 (defmethod task :down
   [args]
-  42)
+  (docker-compose-yml-file! )
+  (-> ^{:out :inherit :err :inherit}
+      ($ docker-compose -f ~my-temp-file down)))
+
+(defmethod task :dbconsole
+  []
+  1)
+
+(defmethod task :help
+  [args]
+  (println "helpa!"))
 
 (defn -main
   "fubar"
   [& args]
-  (let [opts (parse-opts args cli-options)
-        [cmd & rest] (:arguments opts) ]
-    (task [(keyword (or cmd :help))]))
-  ;; (println (yaml/generate-string docker-compose :dumper-options {:flow-style :block}))
-  )
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)
+        [cmd & rest] arguments]
+    (when (seq errors)
+      (println errors)
+      (System/exit 1))
+    (task [(keyword (or cmd :help)) options])))
 
 (apply -main *command-line-args*)
