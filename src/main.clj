a23dbc8b8a04148f18ea0e4b2ebc7f452fba919e
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2024-08-08 14:01:37 +0300

    fixup

--- src/main.clj
+++ src/main.clj
@@ -230,10 +230,10 @@
 (def my-temp-file (fs/delete-on-exit
                    (java.io.File/createTempFile "docker compose-d-" ".yml")))
 
-(defn docker compose-yml [docker compose]
+(defn docker-compose-yml [docker compose]
   (yaml/generate-string docker compose :dumper-options {:flow-style :block}))
 
-(defn docker compose-yml-file! [docker compose-yml]
+(defn docker-compose-yml-file! [docker compose-yml]
   (spit my-temp-file docker compose-yml))
 
 (defn- assemble-app-db
